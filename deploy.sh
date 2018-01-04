76621b3f2856c99d1497146eb4bbea6ca96d51ba
Richard Cheney <richard.cheney@microsoft.com>
2018-01-04 23:34:16 +0000

    Almost...

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-dir=/mnt/c/arm
 template="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.json"
-curl $template
+parameters="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.parameters.json"
+loc=westeurope
+query="properties.output.vpnGatewayIpAddress.value"
+
+rgs=$(curl $template | jq .parameters | grep -i resourceGroup | cut -f4 -d\")
+
+while read rg
+do
+  az group create --location $loc --name $rg
+  hubrg=${hubrg:-$rg}
+done <<< "$rgs"
+
+vpnGatewayIpAddress=$(az group deployment create --resource-group $hubrg --template-uri $template --query $query --output tsv)
+
+print $vpnGatewayIpAddress
+dd

da92220941486530038fe521097c85595037c66d
Richard Cheney <richard.cheney@microsoft.com>
2018-01-05 16:12:28 +0000

    Created error function, and check for existence of az and jq

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
+error()
+{
+  echo "ERROR: $*" >&2
+  exit 1
+}
+
 templateUri="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.json"
 parametersUri="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.parameters.json"
 loc=westeurope
 query="properties.outputs.vpnGatewayIpAddress.value"
 
-## rgs=$(curl $template | jq .parameters | grep -i resourceGroup | cut -f4 -d\")
-## 
-## while read rg
-## do
-  ## az group create --location $loc --name $rg
-  ## [[ -z $hubrg ]] && hubrg=$rg
-## done <<< "$rgs"
-
-[[ -z $hubrg ]] && hubrg=core
+[[ which jq > /dev/null ]] || error "Error: jq must be installed.  Go to https://stedolan.github.io/jq/download/."
+[[ which az > /dev/null ]] || error "Error: az must be installed.  Go to https://aka.ms/GetTheAzureCLI."
 
 parameters=$(curl --silent $parametersUri | jq .parameters)
+[[ -z "$parameters" ]] && error "$parametersUri not found or incorrectly formed."
 
 hubrg=$(jq --raw-output .hub.value.resourceGroup <<< $parameters)
 spokergs=$(jq --raw-output .spokes.value[].resourceGroup <<< $parameters)
 
-echo $hubrg $spokergs
-exit
+echo "Checking or creating resource groups:" >&2
+for rg in $hubrg $spokergs
+do az group create --location $loc --name $rg --output tsv --query name | sed 's/^/\* /1'
+done 
 
-
-
-# [[ -n "$parameters" ]] && { echo "Error: $parameterUri not found or incorrectly formed." >&2; exit 1; }
-
-vpnGatewayIpAddress=$(az group deployment create --resource-group $hubrg --template-uri $templateUri --query $query --output tsv --parameters $parameters)
+echo "Deploying master template..." >&2
+vpnGatewayIpAddress=$(az group deployment create --resource-group $hubrg --template-uri $templateUri --query $query --output tsv --parameters "$parameters")
 
 echo $vpnGatewayIpAddress

d14a3ae14c497ab3f998dc8491fad2a0688963c2
Richard Cheney <richard.cheney@microsoft.com>
2018-01-05 13:04:52 +0000

    Added in spokes

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,18 @@
 template="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.json"
 parameters="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy.parameters.json"
 loc=westeurope
-query="properties.output.vpnGatewayIpAddress.value"
+query="properties.outputs.vpnGatewayIpAddress.value"
 
 rgs=$(curl $template | jq .parameters | grep -i resourceGroup | cut -f4 -d\")
 
 while read rg
 do
   az group create --location $loc --name $rg
-  hubrg=${hubrg:-$rg}
+  [[ -z $hubrg ]] && hubrg=$rg
 done <<< "$rgs"
+
+[[ -z $hubrg ]] && hubrg=core
 
 vpnGatewayIpAddress=$(az group deployment create --resource-group $hubrg --template-uri $template --query $query --output tsv)
 
-print $vpnGatewayIpAddress
-dd
+echo $vpnGatewayIpAddress

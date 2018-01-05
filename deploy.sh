915c3829cd351086414a1b85647d6e1587570106
Richard Cheney <richard.cheney@microsoft.com>
2018-01-05 16:39:45 +0000

    Fixed az and jq checks

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ parametersUri="https://raw.githubusercontent.com/richeney/arm/master/azuredeploy
 loc=westeurope
 query="properties.outputs.vpnGatewayIpAddress.value"
 
-[[ which jq > /dev/null ]] || error "Error: jq must be installed.  Go to https://stedolan.github.io/jq/download/."
-[[ which az > /dev/null ]] || error "Error: az must be installed.  Go to https://aka.ms/GetTheAzureCLI."
+which jq > /dev/null || error "Error: jq must be installed.  Go to https://stedolan.github.io/jq/download/."
+which az > /dev/null || error "Error: az must be installed.  Go to https://aka.ms/GetTheAzureCLI."
 
 parameters=$(curl --silent $parametersUri | jq .parameters)
 [[ -z "$parameters" ]] && error "$parametersUri not found or incorrectly formed."
